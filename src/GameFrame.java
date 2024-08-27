c5a0421e532e02031dcbd43e5fd293d22e8cc0b2
Guilherme <guilhermevianinip@gmail.com>
2024-08-27 17:22:44 -0300

    feat: cria features para a cobra, a comida e uma pontuação

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,13 +1,14 @@
-import javax.swing.JFrame;
+import javax.swing.JFrame; // JFrame é uma classe do Swing que cria janelas
 public class GameFrame extends JFrame
 {
-    public GameFrame() // cria a janela do jogo
+    public GameFrame()
     {
-        this.add(new GamePanel());
+        this.add(new GamePanel()); // cria a janela do jogo
 
         // configs da janela
-        this.setTitle("Snake");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+        this.setTitle("Snake"); //titulo
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //ao fechar a tela, o programa fecha tbm
+        this.setVisible(true);//janela se torna visivel
+        this.setLocationRelativeTo(null);// a tela sempre vai estar centralizada
     }
 }
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -6,26 +6,37 @@ import java.awt.event.*;
 import java.util.Random;
 public class GamePanel extends JPanel implements ActionListener
 {
-    // constantes das configs
+    // constantes das configs e logica do jogo
     private static final int TELA_ALTURA = 500;
     private static final int TELA_LARGURA = 500;
     private static final int TAMANHO_UNITARIO = 25;
-    private static final int UNIDADES = (TELA_ALTURA * TELA_LARGURA);
-    private  Random random; // a comida vai aparecer em posições aleatorias
-}
+    private static final int UNIDADES = (TELA_ALTURA * TELA_LARGURA)/(TAMANHO_UNITARIO * TAMANHO_UNITARIO);
+    private static final int ATRASO = 75; //tempo de atraso de atualização
+
+    //Armazena as coordenadas x e y da cobra
+    private final int[] x = new int[UNIDADES];
+    private final int[] y = new int[UNIDADES];
+
+    //Variáveis de "estado do jogo"
+    private int partesCobra = 3;// tamanho inicial
+    private int comidasConsumidas; //contador de comidas comidas
+    private int comidaX;
+    private int comidaY;
+    private Random random; // posição aleatoria da comida
     public GamePanel()
     {
     random = new Random();
 
     this.setPreferredSize(new Dimension(TELA_LARGURA, TELA_ALTURA));
     this.setBackground(Color.BLACK);
+    this.addKeyListener(new AdaptadorTeclado());
     startGame();
     }
 
     public void startGame()
     {
-        novaComida();
-        rodando = true;
+        novaComida(); // gera a primeira comida
+        rodando = true; // define se o jogo está sendo execultado
     }
     @Override
     public void paintComponent(Graphics g)
@@ -39,7 +50,8 @@ public class GamePanel extends JPanel implements ActionListener
     {
         if(rodando) // comida
         {
-            g.fillOval(comidaX, comidaY, TAMANHO_UNITARIO);
+            g.setColor(Color.yellow);
+            g.fillOval(comidaX, comidaY, TAMANHO_UNITARIO, TAMANHO_UNITARIO);
             for (int i = 0; i < partesCobra; i++)
             {
                 if (i == 0)
@@ -48,13 +60,23 @@ public class GamePanel extends JPanel implements ActionListener
                 } else {
                     g.setColor(new Color(45, 180, 0)); //corpo da cobra (tenho que criar as variaveis para recebar a cor ainda)
                 }
-                g.fillRect(x[i], y[1], TAMANHO_UNITARIO);
+                g.fillRect(x[i], y[1], TAMANHO_UNITARIO, TAMANHO_UNITARIO);
             }
-            g.setColor(Color.red);
+            // desenha pontuacao
+            g.setColor(Color.yellow);
+            g.setFont(new Font("Ink Free", Font.BOLD, 40));
+            FontMetrics metrics = getFontMetrics(g.getFont());
+            g.drawString("Score: ") + comidasConsumidas,(TELA_LARGURA - metrics.charsWidth("Score: " + comidasConsumidas))/2, g.getFont().getSize());
         }else
         {
-            gameOver(g); // preciso criar ainda
+            gameOver(g); // se o jogo não está rodando ele exibe essa tela
         }
     }
+
+    public void novaComida(){
+        comidaX = random.nextInt((int) (TELA_LARGURA/TAMANHO_UNITARIO)) * TAMANHO_UNITARIO;
+        comidaY = random.nextInt((int) (TELA_ALTURA/TAMANHO_UNITARIO)) * TAMANHO_UNITARIO;
+
+    }
 }
-// talvez eu tenha implementado coisas de mais sem testar.
+// arrumar problema de abstração na linha 7 e fazer um metodo timer para o jogo atualizar
